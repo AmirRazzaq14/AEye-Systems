da29b03bcc6ce36e2175eb6ede962ed89fdca9e1
ZuxinChen <116303234+ZuxinChen@users.noreply.github.com>
2026-03-02 19:37:45 -0500

    add prompt file with ollamaAI.py, now can change prompt outside

--- src/main/java/edu/farmingdale/CSC490/Controller/TestController.java
+++ src/main/java/edu/farmingdale/CSC490/Controller/TestController.java
@@ -1,4 +1,4 @@
-package edu.farmingdale.CSC490;
+package edu.farmingdale.CSC490.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
